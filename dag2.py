efc0912e4d5ed8bfbd4193bf5656d53ee6806877
Jojo <jojokoshy@users.noreply.github.com>
2021-03-26 08:41:28 +0800

    updated bash to airflow.operators.bash_operator

--- dag2.py
+++ dag2.py
@@ -29,7 +29,9 @@ from datetime import timedelta
 from airflow import DAG
 
 # Operators; we need this to operate!
-from airflow.operators.bash import BashOperator
+#from airflow.operators.bash import BashOperator
+from airflow.operators.bash_operator import BashOperator
+
 from airflow.utils.dates import days_ago
 
 # [END import_module]
